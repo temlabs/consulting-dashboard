4b359f94d769ba51fa86003ea5b4964a2a64d6b0
temlabs <temilabinjo@hotmail.co.uk>
2022-06-01 07:58:37 +0100

    Added react router dom and a dummy tree in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import { greet } from "./utils/greet";
 import { TestingTailwind } from './components/TestingTailwind'
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 function App(): JSX.Element {
-  return <TestingTailwind text={'Hello this is a test!'}></TestingTailwind>;
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TestingTailwind text={'This is a test!'} />} />
+
+        <Route path="teams" >
+          <Route index element={<TestingTailwind text={'This is Teams'} />} />
+          <Route path=":teamId" element={<TestingTailwind text={'This is Teams with an ID!'} />} />
+        </Route>
+        <Route path="*" element={<TestingTailwind text={'This is an Error Page'} />} />
+      </Routes>
+    </BrowserRouter>
+
+  )
 }
 
 export default App;
