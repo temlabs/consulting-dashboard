4155ad3314813e154d9905020ee65f4e2c5feac0
temlabs <temilabinjo@hotmail.co.uk>
2022-06-01 08:00:24 +0100

    linting and format

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,30 @@
-import { TestingTailwind } from './components/TestingTailwind'
+import { TestingTailwind } from "./components/TestingTailwind";
 
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App(): JSX.Element {
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<TestingTailwind text={'This is a test!'} />} />
+        <Route
+          path="/"
+          element={<TestingTailwind text={"This is a test!"} />}
+        />
 
-        <Route path="teams" >
-          <Route index element={<TestingTailwind text={'This is Teams'} />} />
-          <Route path=":teamId" element={<TestingTailwind text={'This is Teams with an ID!'} />} />
+        <Route path="teams">
+          <Route index element={<TestingTailwind text={"This is Teams"} />} />
+          <Route
+            path=":teamId"
+            element={<TestingTailwind text={"This is Teams with an ID!"} />}
+          />
         </Route>
-        <Route path="*" element={<TestingTailwind text={'This is an Error Page'} />} />
+        <Route
+          path="*"
+          element={<TestingTailwind text={"This is an Error Page"} />}
+        />
       </Routes>
     </BrowserRouter>
-
-  )
+  );
 }
 
 export default App;
